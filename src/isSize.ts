73329383609bfe7e5bcf3fb2e972fb4c2b16dd92
Kamil Mielnik <kamil@kamilmielnik.com>
2023-02-01 13:40:40 +0700

    Make isSize case-insensitive

--- src/isSize.ts
+++ src/isSize.ts
@@ -2,7 +2,12 @@ const isSize = <Size extends string | number | symbol>(
   breakpoints: Record<Size, number>,
   value: unknown,
 ): value is Size => {
-  return typeof value === 'string' && Object.keys(breakpoints).includes(value);
+  return (
+    typeof value === 'string' &&
+    Object.keys(breakpoints)
+      .map((size) => size.toLowerCase())
+      .includes(value.toLowerCase())
+  );
 };
 
 export default isSize;
